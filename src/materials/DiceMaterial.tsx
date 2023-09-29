81938016e1fa35d003901324f07abaf97aeb24cb
SnakeOfMidgaard <hampstamp@gmail.com>
2023-09-29 23:19:31 +0200

    More removal of other dice

--- src/materials/DiceMaterial.tsx
+++ src/materials/DiceMaterial.tsx
@@ -1,34 +1,10 @@
-import { DiceStyle } from "../types/DiceStyle";
-import { GalaxyMaterial } from "./galaxy/GalaxyMaterial";
-import { GemstoneMaterial } from "./gemstone/GemstoneMaterial";
-import { GlassMaterial } from "./glass/GlassMaterial";
-import { IronMaterial } from "./iron/IronMaterial";
-import { NebulaMaterial } from "./nebula/NebulaMaterial";
-import { SunriseMaterial } from "./sunrise/SunriseMaterial";
-import { SunsetMaterial } from "./sunset/SunsetMaterial";
-import { WalnutMaterial } from "./walnut/WalnutMaterial";
+
 import { MyzbaseMaterial } from "./myzbase/MyzbaseMaterial";
 import { MyzgearMaterial } from "./myzgear/MyzgearMaterial";
 import { MyzskillMaterial } from "./myzskill/MyzskillMaterial";
 
 export function DiceMaterial({ diceStyle }: { diceStyle: DiceStyle }) {
   switch (diceStyle) {
-    case "GALAXY":
-      return <GalaxyMaterial />;
-    case "GEMSTONE":
-      return <GemstoneMaterial />;
-    case "GLASS":
-      return <GlassMaterial />;
-    case "IRON":
-      return <IronMaterial />;
-    case "NEBULA":
-      return <NebulaMaterial />;
-    case "SUNRISE":
-      return <SunriseMaterial />;
-    case "SUNSET":
-      return <SunsetMaterial />;
-    case "WALNUT":
-      return <WalnutMaterial />;
     case "MYZBASE":
       return <MyzbaseMaterial />;
     case "MYZGEAR":
